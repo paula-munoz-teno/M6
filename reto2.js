7d021e9db8b101d0a680974ecb594ec13758891e
Paula-Munoz-Teno <paulamtlaboral@gmail.com>
2024-11-29 12:49:12 +0100

    corrección retos dia2M6

--- reto2.js
+++ reto2.js
@@ -19,23 +19,75 @@ let studentsDocument = new students({
 });
 
 
+
+
+// console.log("las notas de Marcos son:",resp.marks[0].mark, "y", resp.marks[1].mark)
+//     console.log("las asignaturas de Marcos son:",resp.marks[0].subjects[0].title, "y",resp.marks[1].subjects[0].title )
+//     console.log("los profesores de Marcos son:",
+//         resp.marks[0].subjects[0].teachers[0].firstName,
+//         resp.marks[0].subjects[0].teachers[0].lastName, "y",
+//         resp.marks[1].subjects[0].teachers[0].firstName,
+//         resp.marks[1].subjects[0].teachers[0].lastName
+//     )
+
+// array.forEach(function(element, index, array) {
+//     // Código a ejecutar para cada elemento
+// });
+
+
 studentsDocument.save()
-.then(resp => {
-    console.log("Documento guardado correctamente")
-    console.log(resp)
-    console.log("las notas de Marcos son:",resp.marks[0].mark, "y", resp.marks[1].mark)
-    console.log("las asignaturas de Marcos son:",resp.marks[0].subjects[0].title, "y",resp.marks[1].subjects[0].title )
-    console.log("los profesores de Marcos son:",
-        resp.marks[0].subjects[0].teachers[0].firstName,
-        resp.marks[0].subjects[0].teachers[0].lastName, "y",
-        resp.marks[1].subjects[0].teachers[0].firstName,
-        resp.marks[1].subjects[0].teachers[0].lastName
-    )
+.then(function(resp) {
+    console.log("Documento guardado correctamente");
+    console.log(resp);
+
+    // notas
+    console.log("Las notas de Marcos son:");
+    resp.marks.forEach(function(mark) {
+        console.log(mark.mark);
+    });
+
+    // Mostrar  las asignaturas
+    console.log("Las asignaturas de Marcos son:");
+    resp.marks.forEach(function(mark) {
+        mark.subjects.forEach(function(subject) {
+            console.log(subject.title);
+        });
+    });
+
+    // Mostrar los profesores
+    console.log("Los profesores de Marcos son:");
+    resp.marks.forEach(function(mark) {
+        mark.subjects.forEach(function(subject) {
+            subject.teachers.forEach(function(teacher) {
+                console.log(teacher.firstName + " " + teacher.lastName);
+            });
+        });
+    });
+})
+.catch(function(error) {
+    console.log("Error: " + error);
+});
+
+
+
+
+// studentsDocument.save()
+// .then(resp => {
+//     console.log("Documento guardado correctamente")
+//     console.log(resp)
+//     console.log("las notas de Marcos son:",resp.marks[0].mark, "y", resp.marks[1].mark)
+//     console.log("las asignaturas de Marcos son:",resp.marks[0].subjects[0].title, "y",resp.marks[1].subjects[0].title )
+//     console.log("los profesores de Marcos son:",
+//         resp.marks[0].subjects[0].teachers[0].firstName,
+//         resp.marks[0].subjects[0].teachers[0].lastName, "y",
+//         resp.marks[1].subjects[0].teachers[0].firstName,
+//         resp.marks[1].subjects[0].teachers[0].lastName
+//     )
     
-})
-.catch(error => {
-    console.log("Error: " + error)
-})
+// })
+// .catch(error => {
+//     console.log("Error: " + error)
+// })
 
 
 
